c4a612439cc458b2f28f952c448258b682ed779e
Agung Yuliyanto <agung.96tm@gmail.com>
2023-09-08 11:53:02 +0700

    hide password from response

--- models/user.go
+++ models/user.go
@@ -11,11 +11,11 @@ import (
 type User struct {
 	gorm.Model
 	Username string `gorm:"column:username;size:64;not null;index;" json:"username" validate:"required"`
-	Password string `gorm:"column:password;not null;" json:"password" validate:"required"`
+	Password string `gorm:"column:password;not null;" json:"-" validate:"required"`
 	Email    string `gorm:"column:email;not null;" json:"email" validate:"required"`
 
-	Profile Profile `gorm:"foreignKey:UserID;references:ID"`
-	Post    []Post  `gorm:"foreignKey:UserID;references:ID"`
+	Profile Profile `gorm:"foreignKey:UserID;references:ID" json:"profile"`
+	Posts   []Post  `gorm:"foreignKey:UserID;references:ID" json:"posts"`
 }
 
 type Users = []User
